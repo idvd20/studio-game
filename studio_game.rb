a3a021e493c011b50bce3f675fa0c937494cd0cf
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-22 20:15:11 +0800

    First commit

---
+++ studio_game.rb
@@ -0,0 +1,8 @@
+player1 = 'larry'
+player2 = 'curly'
+player3 = 'more'
+health1 = 60
+
+puts "#{player1}'s health is #{health1 / 9.0}"
+
+puts "Players: \n\t#{player1} \n\t#{player2} \n\t#{player3}"
