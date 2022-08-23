fab4b22c88ebcca05611ffe2159010141eef3a74
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-23 21:48:58 +0800

    Separate source files

---
+++ game.rb
@@ -0,0 +1,27 @@
+require_relative 'player'
+
+class Game
+    attr_reader :title
+
+    def initialize(title)
+        @title = title.capitalize
+        @players = []
+    end
+
+    def add_player(player)
+        @players << player
+    end
+
+    def play
+        puts "There are #{@players.size} in #{@title}:"
+        @players.each do |player|
+            puts player
+        end
+        @players.each do |player|
+            player.blam
+            player.w00t
+            player.w00t
+            puts player
+        end
+    end
+end
