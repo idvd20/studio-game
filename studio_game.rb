589c5f922d0f9f71d2a3d34170219deb146a04ec
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-23 11:45:15 +0800

    Methods practice

--- studio_game.rb
+++ studio_game.rb
@@ -1,28 +1,12 @@
-player1 = 'larry'
-player2 = 'curly'
-player3 = 'moe'
-player4 = 'shemp'
-health1 = 60
-health2 = 125
-health3 = 100
-health4 = 90
+def time
+    Time.new.strftime("%H:%M:%S")
+end
 
-puts "#{player1.capitalize}'s health is #{health1}"
-puts "#{player2.upcase}'s health is #{health2}"
+def say_hello(name, health=100)
+    "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+end
 
-# health2 = health1
-# puts "#{player2.upcase}'s health is #{health2}"
-
-# health1 = 30
-# puts "#{player1.capitalize}'s health is #{health1}"
-# puts "#{player2.upcase}'s health is #{health2}"
-
-puts "#{player3.capitalize}'s health is #{health3}".center(50, '*')
-
-puts "#{player4.capitalize.ljust(30, '.')} #{health4} health"
-
-time = Time.new.strftime("%A %m/%d/%Y at %I:%M%p")
-
-puts "The game started on #{time}"
-
-# puts "Players: \n\t#{player1} \n\t#{player2} \n\t#{player3}"
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
