0f28026bc8485d46c3330f6adbddb5b639ad8940
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-23 14:38:11 +0800

    Attribute Practice

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,11 @@
 class Player
+    attr_reader :health
+    attr_accessor :name
+
+    def name=(new_name)
+        @name = new_name.capitalize
+    end
+
     def initialize(name, health=100)
         @name = name.capitalize
         @health = health
@@ -14,8 +21,12 @@ class Player
         puts "#{@name} got w00ted!"
     end
 
+    def score
+        @health + @name.length
+    end
+
     def to_s
-        "I'm #{@name} with a health of #{@health}"
+        "I'm #{@name} with a health of #{@health} and a score of #{score}"
     end
 end
 
@@ -23,9 +34,6 @@ player1 = Player.new("moe")
 puts player1
 player2 = Player.new("larry", 60)
 puts player2
+player2.name = "lawrence"
+puts player2
 player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
