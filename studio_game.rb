574f655f02c2fca8c364898186153eb0cb7b599a
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-08-23 12:37:50 +0800

    Classes practice

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,31 @@
-def time
-    Time.new.strftime("%H:%M:%S")
+class Player
+    def initialize(name, health=100)
+        @name = name.capitalize
+        @health = health
+    end
+
+    def blam
+        @health -= 10
+        puts "#{@name} got blammed!"
+    end
+
+    def w00t
+        @health += 15
+        puts "#{@name} got w00ted!"
+    end
+
+    def to_s
+        "I'm #{@name} with a health of #{@health}"
+    end
 end
 
-def say_hello(name, health=100)
-    "I'm #{name.capitalize} with a health of #{health} as of #{time}"
-end
-
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player1 = Player.new("moe")
+puts player1
+player2 = Player.new("larry", 60)
+puts player2
+player3 = Player.new("curly", 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
